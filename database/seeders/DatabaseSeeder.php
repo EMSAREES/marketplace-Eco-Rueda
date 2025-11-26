9437e9e157e98d16c1e0afe36be8e91d046c1b46
EMSAREES <lobomategamer@gmail.com>
2025-11-25 20:01:01 -0700

    Ignorar carpeta de imágenes de productos locales

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,72 +17,74 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Usuario admin
-        User::create([
-            'name' => 'Admin',
-            'email' => 'admin@tienda.com',
-            'password' => Hash::make('password'),
-            'role' => 'admin',
-            'is_verified' => true
-        ]);
+        $this->call(UserSeeder::class);
 
-        // Vendedor de prueba
-        $vendor = User::create([
-            'name' => 'Vendedor Sillas Sostenibles',
-            'email' => 'vendor@tienda.com',
-            'password' => Hash::make('password'),
-            'role' => 'vendor',
-            'is_verified' => true,
-            'phone' => '555-1234'
-        ]);
+        // // Usuario admin
+        // User::create([
+        //     'name' => 'Admin',
+        //     'email' => 'admin@tienda.com',
+        //     'password' => Hash::make('password'),
+        //     'role' => 'admin',
+        //     'is_verified' => true
+        // ]);
 
-        // Productos de ejemplo
-        $products = [
-            [
-                'name' => 'Silla Moderna Eco - Madera Reciclada',
-                'description' => 'Hermosa silla hecha con madera 100% reciclada. Diseño moderno y cómodo.',
-                'price' => 45.99,
-                'stock' => 10,
-                'material' => 'Madera reciclada',
-                'color' => 'Natural'
-            ],
-            [
-                'name' => 'Silla Plástico Reutilizado',
-                'description' => 'Silla ligera y resistente hecha con plástico PET reutilizado.',
-                'price' => 35.50,
-                'stock' => 15,
-                'material' => 'Plástico reciclado',
-                'color' => 'Gris'
-            ],
-            [
-                'name' => 'Silla de Comedor Vintage',
-                'description' => 'Silla estilo vintage restaurada con materiales sostenibles.',
-                'price' => 55.00,
-                'stock' => 8,
-                'material' => 'Madera y tela reciclada',
-                'color' => 'Castaño'
-            ]
-        ];
+        // // Vendedor de prueba
+        // $vendor = User::create([
+        //     'name' => 'Vendedor Sillas Sostenibles',
+        //     'email' => 'vendor@tienda.com',
+        //     'password' => Hash::make('password'),
+        //     'role' => 'vendor',
+        //     'is_verified' => true,
+        //     'phone' => '555-1234'
+        // ]);
 
-        foreach ($products as $product) {
-            Product::create([
-                'vendor_id' => $vendor->id,
-                ...$product
-            ]);
-        }
+        // // Productos de ejemplo
+        // $products = [
+        //     [
+        //         'name' => 'Silla Moderna Eco - Madera Reciclada',
+        //         'description' => 'Hermosa silla hecha con madera 100% reciclada. Diseño moderno y cómodo.',
+        //         'price' => 45.99,
+        //         'stock' => 10,
+        //         'material' => 'Madera reciclada',
+        //         'color' => 'Natural'
+        //     ],
+        //     [
+        //         'name' => 'Silla Plástico Reutilizado',
+        //         'description' => 'Silla ligera y resistente hecha con plástico PET reutilizado.',
+        //         'price' => 35.50,
+        //         'stock' => 15,
+        //         'material' => 'Plástico reciclado',
+        //         'color' => 'Gris'
+        //     ],
+        //     [
+        //         'name' => 'Silla de Comedor Vintage',
+        //         'description' => 'Silla estilo vintage restaurada con materiales sostenibles.',
+        //         'price' => 55.00,
+        //         'stock' => 8,
+        //         'material' => 'Madera y tela reciclada',
+        //         'color' => 'Castaño'
+        //     ]
+        // ];
 
-        // Cliente de prueba
-        User::create([
-            'name' => 'Cliente Prueba',
-            'email' => 'customer@tienda.com',
-            'password' => Hash::make('password'),
-            'role' => 'customer',
-            'phone' => '555-5678',
-            'address' => 'Calle Principal 123',
-            'city' => 'Guasave',
-            'state' => 'Sinaloa',
-            'country' => 'México',
-            'postal_code' => '81250'
-        ]);
+        // foreach ($products as $product) {
+        //     Product::create([
+        //         'vendor_id' => $vendor->id,
+        //         ...$product
+        //     ]);
+        // }
+
+        // // Cliente de prueba
+        // User::create([
+        //     'name' => 'Cliente Prueba',
+        //     'email' => 'customer@tienda.com',
+        //     'password' => Hash::make('password'),
+        //     'role' => 'customer',
+        //     'phone' => '555-5678',
+        //     'address' => 'Calle Principal 123',
+        //     'city' => 'Guasave',
+        //     'state' => 'Sinaloa',
+        //     'country' => 'México',
+        //     'postal_code' => '81250'
+        // ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,13 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $vendor = User::create([
+            'name' => 'Vendedor Sillas Sostenibles',
+            'email' => 'vendor@tienda.com',
+            'password' => Hash::make('password'),
+            'role' => 'vendor',
+            'is_verified' => true,
+            'phone' => '555-1234'
+        ]);
     }
 }
